9602f68c54a40bd858e1a4d1080fbcd6fafae548
KashishKhunger <Kashish.Khungar@gmail.com>
2025-04-13 01:22:44 +0530

    13-April-2025 * 13th coz it's 1:22 AM :P * ** Fifth Program **

--- src/CodingPracticeExercise_250plus/codcon_004_Class_Person.java
+++ src/CodingPracticeExercise_250plus/codcon_004_Class_Person.java
@@ -15,40 +15,39 @@ public class codcon_004_Class_Person {
         System.out.println(N);
 
     }
+}
 
-     static class Person{
-        String Name;
-        byte Age;
-        String Address;
-        String Profession;
-        String Company;
-        double Salary;
-        Boolean is_Married;
-        Boolean has_Dependents;
-        int number_of_Dependents;
-        byte years_of_experience;
+class Person{
+    String Name;
+    byte Age;
+    String Address;
+    String Profession;
+    String Company;
+    double Salary;
+    Boolean is_Married;
+    Boolean has_Dependents;
+    int number_of_Dependents;
+    byte years_of_experience;
 
-        // 1.Without Parameters and Without Return Type
-        void run(){
-                System.out.println("Person is Running");
-        }
-
-        // 2.Without Parameters but With Return Type
-        public double inHandSalary() {
-            return Salary;
-        }
-
-        // 3.With Parameters and Without Return Type
-
-        public void setAddress(String address) {
-            Address = address;
-        }
-
-        // 4. With Parameters and With Return Type
-        byte number_of_Years_without_Job(byte Age ){
-            return (byte) (Age-years_of_experience);
-        }
-
-
+    // 1.Without Parameters and Without Return Type
+    void run(){
+        System.out.println("Person is Running");
     }
+
+    // 2.Without Parameters but With Return Type
+    public double inHandSalary() {
+        return Salary;
+    }
+
+    // 3.With Parameters and Without Return Type
+
+    public void setAddress(String address) {
+        Address = address;
+    }
+
+    // 4. With Parameters and With Return Type
+    byte number_of_Years_without_Job(byte Age ){
+        return (byte) (Age-years_of_experience);
+    }
+
 }
---
+++ src/CodingPracticeExercise_250plus/codcon_005_Constructor_Person.java
@@ -0,0 +1,70 @@
+package CodingPracticeExercise_250plus;
+
+public class codcon_005_Constructor_Person {
+    public static void main(String[] args) {
+        Person1 P1 = new Person1();
+        System.out.println(P1.Name);
+        System.out.println(P1.Age);
+        System.out.println(P1.Address);
+        System.out.println(P1.Phone);
+        System.out.println(P1.is_Male);
+
+        Person1 P2 = new Person1("Ananya" ,"XYZ");
+        P2.printDetails();
+
+        Person1 P3 = new Person1("Rohan", 4 , 47247357);
+        P3.printDetails();
+    }
+}
+
+class Person1{
+    String Name;
+    int Age;
+    String Address;
+    int Phone;
+    Boolean is_Male;
+
+    // Default Constructor
+    Person1(){
+        Name = "Default";
+    }
+    //Parameterised Constructor
+    Person1(String name, int age,int phone){
+
+        Name = name ;
+        Age = age;
+        Phone = phone;
+
+    }
+    //PC Type 2
+    Person1(String name, String add){
+        Name = name ;
+        Address = add;
+    }
+
+
+    //Behaviours/Functions
+    void eat(){
+        System.out.println(Name + " eats");
+    }
+    void sleep(){
+        System.out.println(Name + " sleeps");
+    }
+    void walk(){
+        System.out.println(Name + " walks");
+    }
+    void doHome(){
+        System.out.println(Name + " does Homework");
+    }
+    void printDetails(){
+        System.out.println("Name:" + Name);
+        System.out.println("Age:" + Age);
+        System.out.println("Phone:" + Phone);
+        System.out.println("Address:" + Address);
+        System.out.println("Gender is Male:" + is_Male);
+        walk();
+    }
+
+
+
+}
